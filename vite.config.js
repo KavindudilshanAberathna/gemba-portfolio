b8a6a63bcde70e2e2f6cb0538cdd349094e2e0de
Kavindu Dilshan <kavindudilshanz3120@gmail.com>
2025-11-15 01:03:30 -0800

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  // GitHub Pages base path
+  base: "/Gemba-portfolio/",
+
+  // Output folder must be docs for GitHub Pages
   build: {
-    outDir: "build",
+    outDir: "docs",
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
